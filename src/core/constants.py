0e15eed21d7c67b110556b0f52dd4362b6a23f25
argamManukyan <62460275+argamManukyan@users.noreply.github.com>
2024-01-09 17:30:19 +0400

    added new constant

--- src/core/constants.py
+++ src/core/constants.py
@@ -1,8 +1,11 @@
 from enum import Enum
+from typing import NewType, Any
+
+DbBaseModel = NewType("T", Any)
+
 # Project constants
 
 MAIN_OPERATIONS = ["create", "read", "update", "delete"]
-
 
 # Auth constants
 
@@ -10,7 +13,7 @@ PASSWORD_CHECKER_PATTERN = "^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{6,}$"
 TOKEN_PARTITIONS_NUMBER = 3
 OTP_GENERATION_DIVISION = 100_0000
 
+
 class TokenTypes(Enum):
     ACCESS = "access"
     REFRESH = "refresh"
-
